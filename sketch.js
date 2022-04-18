fd3cd029f1706a996c2f998499f3ba6a930a68f7
fajarbaru-ma2213 <103625740+fajarbaru-ma2213@users.noreply.github.com>
2022-04-19 06:28:31 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,3 @@
-// data gambar
-let bgp;
-let petaa;
-let profil;
-let logo;
-let bawah;
-
 //Data Digram Pie
 let dataPekerjaan;
 let urlKerja = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRr5G1-B5yF6Dn5OigF0aCSmFl8Lk68j9wsiY7l6J-3MlxxfdqLgfn3JNTIjl8wK8F5UGf6JJBdqgAk/pub?gid=0&single=true&output=csv"
@@ -17,18 +10,13 @@ let urlSarana = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTkyC_KEpkqNl8x
 let dataUsia;
 let urlUsia = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRWbDnkw2K5DvYLuQ5MPZ13633u2gJ-OaOiZJUul-sVxfOA1IMPyIR4Qxzl2ND_iDyyaVZTzHAi5Vdj/pub?gid=0&single=true&output=csv"
 
-//font tulisan
+
 let RobotoMonoSemiBold;
 
 
-
 function preload(){
+   
     RobotoMonoSemiBold = loadFont('RobotoMono-Bold.ttf')
-    bgp = loadImage('bg.jpg')
-    petaa = loadImage('peta.png')
-    profil = loadImage('organisasi.jpg')
-    logo = loadImage('Logo.png')
-    bawah = loadImage('bawah.png')
     dataPekerjaan = loadTable(urlKerja, 'csv', 'header');
     dataJenisKelamin = loadTable(urlJenis, 'csv', 'header');
     dataPendidikan = loadTable(urlPendidikan, 'csv', 'header');
@@ -38,68 +26,25 @@ function preload(){
 
 
 function setup() {
-  createCanvas(1600,3000);  
+    createCanvas(1290,1000);
+ 
+    
   }
   
   
   
-function draw() {
+  function draw() {
     background('white');
     noStroke()
+    textSize(30)
+        textFont(RobotoMonoSemiBold)
+        fill('black')
+        text("DASBOARD DESA FAJAR BARU",500,25)
 
-    //background
-    image(bgp,0,0,1600,900);
-    fill(61,56,80,200);
-    rect(0,0,1600,900);
 
-    //Peta Fajar Baru
-    fill('black')
-    textFont(RobotoMonoSemiBold)
-    textSize (17)
-    text ("Peta Fajar Baru",220,950)
-    image(petaa,100,900,500,550)
-
-    //Struktur Kelurahan
-    
-    image(profil,350,1300,800,550)
-
-    //Logo
-    image(logo,530,10,400,180)
-
-    //bawah
-    image(bawah,10,2800,1500,200)
-
-    //Fajar Baru
-    fill('white')
-    textFont(RobotoMonoSemiBold)
-    textSize (64)
-    text ("Desa Fajar Baru",450,450)
-
-    //Info Desa Fajar Baru
-  fill('white')
-  rect(800,982,790,300);
-  textSize(20)
-  textFont(RobotoMonoSemiBold)
-  fill('black')
-  text("Tentang Desa",910,950)
-  
-  //Paragraf
-  textSize(17)
-  textFont(RobotoMonoSemiBold)
-  text("Pada mulanya Desa Fajar Baru merupakan bagian dari Desa Karang",650,1020)
-  text("Anyar, yang pada tahun 1959 disebut dengan susukan Fajar Baru. ",650,1040)
-  text("Selanjutnya tahun 1960 memisahkan diri dari Desa Karang Anyar dengan ",650,1060+2)
-  text("kades A. Sastro Rejo. Tahun 1965 kembali menggabungkan diri dengan ",650,1080+3)
-  text("Desa Karang Anyar yang waktu itu dipimpin Kades Hadi Sumarto. ",650,1110+4)
-  text("Padatahun 1968-1984 Desa Karang Anyar dipimpin oleh Kades Aliesan Hakim. ",650,1130+5)
-  text("Pada tahun tersebut diadakan pemekaran desa dan sebagai penjabat Kades ",650,1150+6)
-  text("Bapak Aliesan membuat sebutan Desa Fajar Baru Kecamatan Jati Agung  ",650,1170+7)
-  text("Kabupaten Lampung Selatan dan pada tanggal 20 Oktober 1986 ditetapkan ",650,1190+8)
-  text("menjadi Desa Persiapan dengan Kepala Desa Persiapan bapak Aliesan dan ",650,1210+9)
-  text("menjabat selama 5 tahun",650,1230+10)
  
 
-  //Input sudut dari setiap data
+  //Input sudut setiap data
     anglespek = dataPekerjaan.getColumn('x')
     pieChartpek(200, anglespek);
     anglesjen = dataJenisKelamin.getColumn('x')
@@ -112,14 +57,14 @@ function draw() {
     pieChartus(200, anglesus);
   
   }
-    //Chart Pekerjaan
+
     function pieChartpek(diameterpek, dataPekerjaan) {
         let lastAnglepek = 0;
         var piecolorpek = ['#FA8072','#F4A460 ','#2E8B57 ','#A0522D ','#4682B4 ', '#87CEEB','#6A5ACD']
         for (var i = 0; i < dataPekerjaan.length; i++) {
           fill(piecolorpek[i])
           arc(
-            720,2000,
+            710,200,
             diameterpek,
             diameterpek,
             lastAnglepek,
@@ -127,52 +72,51 @@ function draw() {
           );
           lastAnglepek += radians(anglespek[i]);
         }
-        
+        //Data Pekerjaan
         fill('white')
-        //rect(545,2953,350,450)
+        
         textSize(18)
         textFont(RobotoMonoSemiBold)
         fill('black')
-        text("Data Pekerjaan",660,1850)
+        text("Data Pekerjaan",630,70)
         
         textSize(12)
         textFont(RobotoMonoSemiBold)
-        text("Karyawan                    = 963",600,2130)
-        text("Wiraswasta                  = 368",600,2150)
-        text("Petani                      = 527",600,2170)
-        text("Peternak                    = 80",600,2190)
-        text("Buruh Tani                  = 869",600,2210)
-        text("Pengrajin                   = 50",600,2230)
-        text("Lainnya                     = 379",600,2250)
+        text("Karyawan                    = 963",600,330)
+        text("Wiraswasta                  = 368",600,350)
+        text("Petani                      = 527",600,370)
+        text("Peternak                    = 80",600,390)
+        text("Buruh Tani                  = 869",600,410)
+        text("Pengrajin                   = 50",600,430)
+        text("Lainnya                     = 379",600,450)
         
 
         //Warna
         fill(piecolorpek[0]) ;
-        ellipse(780,2125,10,10);
+        ellipse(780,325,10,10);
         fill(piecolorpek[1]) ;
-        ellipse(780,2145,10,10);
+        ellipse(780,345,10,10);
          fill(piecolorpek[2]) ;
-        ellipse(780,2165,10,10);
+        ellipse(780,365,10,10);
          fill(piecolorpek[3]) ;
-        ellipse(780,2185,10,10);
+        ellipse(780,385,10,10);
          fill(piecolorpek[4]) ;
-        ellipse(780,2205,10,10);
+        ellipse(780,405,10,10);
          fill(piecolorpek[5]) ;
-        ellipse(780,2225,10,10);
+        ellipse(780,425,10,10);
          fill(piecolorpek[5]) ;
-        ellipse(780,2245,10,10);
+        ellipse(780,445,10,10);
          fill(piecolorpek[6]) ;
         
       }
       
-      //chart Jenis Kelamin
       function pieChartjen(diameterjen, dataJenisKelamin) {
         let lastAnglejen = 0;
         var piecolorjen = ['#94D2BD ','#EE9B00']
         for (var i = 0; i < dataJenisKelamin.length; i++) {
           fill(piecolorjen[i])
           arc(
-            275,2000,
+            260,200,
             diameterjen,
             diameterjen,
             lastAnglejen,
@@ -182,33 +126,33 @@ function draw() {
         }
         //Data Jenis Kelamin
         fill('white')
-        //rect(545,2953,350,450)
+       
         textSize(18)
         textFont(RobotoMonoSemiBold)
         fill('black')
-        text("Data Jenis Kelamin",195,1850)
+        text("Data Jenis Kelamin",160,70)
         
         textSize(12)
         textFont(RobotoMonoSemiBold)
-        text("Pria                = 3890",170,2130)
-        text("Wanita              = 4054",170,2150)
-      
-        // warna
+        text("Pria                = 3890",170,330)
+        text("Wanita              = 4054",170,350)
+        
+
+        //penanda warna
         fill(piecolorjen[0]) ;
-        ellipse(300,2130,10);
+        ellipse(300,325,10);
         fill(piecolorjen[1]) ;
-        ellipse(300,2150,10);
+        ellipse(300,345,10);
          
     }
 
-    // Chart Pendidikan
     function pieChartpen(diameterpen, dataPendidikan) {
       let lastAnglepen = 0;
       var piecolorpen = ['#D8BFD8','#005F73 ','#0A9396 ','#94D2BD ','#E9D8A6 ', '#EE9B00','#CA6702']
       for (var i = 0; i < dataPendidikan.length; i++) {
         fill(piecolorpen[i])
         arc(
-          1200,2000,
+          1190,200,
           diameterpen,
           diameterpen,
           lastAnglepen,
@@ -216,40 +160,40 @@ function draw() {
         );
         lastAnglepen += radians(anglespen[i]);
       }
-      
+      //Data Pendidikan
       fill('white')
       //rect(545,2953,350,450)
       textSize(18)
       textFont(RobotoMonoSemiBold)
       fill('black')
-      text("Data Pendidikan",1150,1850)
-  
+      text("Data Pendidikan",1100,70)
+      
       textSize(12)
       textFont(RobotoMonoSemiBold)
-      text("TK                         = 190",1100,2130)
-      text("SD                         = 1237",1100,2150)
-      text("SMP                        = 1052",1100,2170)
-      text("SMA/SMU                    = 974",1100,2190)
-      text("Akademi/D1-D3              = 203",1100,2210)
-      text("Sarjana                    = 92",1100,2230)
-      text("PascaSarjana               = 18",1100,2250)
+      text("TK                         = 190",1050,330)
+      text("SD                         = 1237",1050,350)
+      text("SMP                        = 1052",1050,370)
+      text("SMA/SMU                    = 974",1050,390)
+      text("Akademi/D1-D3              = 203",1050,410)
+      text("Sarjana                    = 92",1050,430)
+      text("PascaSarjana               = 18",1050,450)
       
 
-      //warna
+      //penanda warna
       fill(piecolorpen[0]) ;
-      ellipse(1275,2125,10,10);
+      ellipse(1225,325,10,10);
       fill(piecolorpen[1]) ;
-      ellipse(1275,2145,10,10);
+      ellipse(1225,345,10,10);
        fill(piecolorpen[2]) ;
-      ellipse(1275,2165,10,10);
+      ellipse(1225,365,10,10);
        fill(piecolorpen[3]) ;
-      ellipse(1275,2185,10,10);
+      ellipse(1225,385,10,10);
        fill(piecolorpen[4]) ;
-      ellipse(1275,2205,10,10);
+      ellipse(1225,405,10,10);
        fill(piecolorpen[5]) ;
-      ellipse(1275,2225,10,10);
+      ellipse(1225,425,10,10);
        fill(piecolorpen[6]) ;
-      ellipse(1275,2245,10,10);
+      ellipse(1225,445,10,10);
    
     }
 
@@ -260,7 +204,7 @@ function draw() {
       for (var i = 0; i < dataSarana.length; i++) {
         fill(piecolorlul[i])
         arc(
-          480,2420,
+          480,700,
           diameterlul,
           diameterlul,
           lastAnglelul,
@@ -268,32 +212,36 @@ function draw() {
         );
         lastAnglelul += radians(angleslul[i]);
       }
-      
+      //Data Sarana
       fill('white')
       //rect(545,2953,350,450)
       textSize(18)
       textFont(RobotoMonoSemiBold)
       fill('black')
-      text("Sarana Prasarana",400,2300)
+      text("Sarana Prasarana",400,580)
       
       textSize(12)
       textFont(RobotoMonoSemiBold)
-      text("Prasarana Kesehatan        = 6",330,2550)
-      text("Prasarana Pendidikan       = 12",330,2570)
-      text("Prasarana Ibadah           = 26",330,2590)
-      text("Prasarana Umum             = 5",330,2610)
+      text("Prasarana Kesehatan        = 6",380,850)
+      text("Prasarana Pendidikan       = 12",380,870)
+      text("Prasarana Ibadah           = 26",380,890)
+      text("Prasarana Umum             = 5",380,910)
       
-      // warna
-      fill(piecolorlul[0]) ;
-      ellipse(505,2545,10,10);
-      fill(piecolorlul[1]) ;
-      ellipse(505,2565,10,10);
-       fill(piecolorlul[2]) ;
-      ellipse(505,2585,10,10);
-       fill(piecolorlul[3]) ;
-      ellipse(505,2605,10,10);
+      
 
+      //penanda warna
+      fill(piecolorlul[0]) ;
+      ellipse(555,845,10,10);
+      fill(piecolorlul[1]) ;
+      ellipse(555,865,10,10);
+       fill(piecolorlul[2]) ;
+      ellipse(555,885,10,10);
+       fill(piecolorlul[3]) ;
+      ellipse(555,905,10,10);
+     
+   
     }
+
     //Chart Usia
     function pieChartus(diameterus, dataUsia) {
       let lastAngleus = 0;
@@ -301,7 +249,7 @@ function draw() {
       for (var i = 0; i < dataUsia.length; i++) {
         fill(piecolorus[i])
         arc(
-          880,2420,
+          880,700,
           diameterus,
           diameterus,
           lastAngleus,
@@ -309,27 +257,27 @@ function draw() {
         );
         lastAngleus += radians(anglesus[i]);
       }
+      //Data Usia
       fill('white')
-      //rect(545,2953,350,450)
+      
       textSize(18)
       textFont(RobotoMonoSemiBold)
       fill('black')
-      text("Data Usia",830,2300)
+      text("Data Usia",830,580)
       
       textSize(12)
       textFont(RobotoMonoSemiBold)
-      text("0-15 Tahun               = 138",730,2550)
-      text("15-65 Tahun              = 5629",730,2570)
-      text("65 + Tahun               = 584",730,2590)
+      text("0-15 Tahun               = 267",780,850)
+      text("15-65 Tahun              = 1373",780,870)
+      text("65 + Tahun               = 1689",780,890)
       
       
-      // warna
+      //penanda warna
       fill(piecolorus[0]) ;
-      ellipse(900,2545,10,10);
+      ellipse(950,845,10,10);
       fill(piecolorus[1]) ;
-      ellipse(900,2565,10,10);
+      ellipse(950,865,10,10);
        fill(piecolorus[2]) ;
-      ellipse(900,2585,10,10);
+      ellipse(950,885,10,10);
     
     }
-  
